844932f1f85c499bc13ba43dd43d46327d4a6b71
hientran-sudo <hien.tran@student.fairfield.edu>
2023-12-28 14:07:02 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,8 @@ import {NgForm} from "@angular/forms";
 export class AppComponent {
   title = 'bootcamp';
 
-  onSubmit(){
-    
+  onSubmit(userDetails : NgForm){
+    console.log("Form Submitited " + userDetails.value.fName + " " + userDetails.value.lName);
+
   }
 }
